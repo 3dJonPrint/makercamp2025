b5d6d84deadc3b57978825602b67647d6274e064
3dJonPrint <jonas.spitzmueller@outlook.de>
2025-08-08 11:03:31 +0200

    fix: simplify joystick value printing by removing axis calculations

--- test_cont.py
+++ test_cont.py
@@ -2,8 +2,4 @@ from xbox360controller import Xbox360Controller
 
 with Xbox360Controller(0, axis_threshold=0) as joy:
   while True:
-    l_x = int(joy.axis_l.x)
-    l_y = int(joy.axis_l.y)
-    l_x *= 10
-    l_y *= 10
-    print(l_x, l_y)
+    print(joy)
