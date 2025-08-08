087225dc3d6f84a3b49726fb44074028a487993d
3dJonPrint <jonas.spitzmueller@outlook.de>
2025-08-08 09:10:30 +0200

    i don't now what i'm doing here

--- test_cont.py
+++ test_cont.py
@@ -2,8 +2,8 @@ from xbox360controller import Xbox360Controller
 
 with Xbox360Controller(0, axis_threshold=0) as joy:
   while True:
-    l_x = 0.5 - joy.axis_l.x
-    l_y = 0.5 + joy.axis_l.y
+#    l_x = 0.5 - joy.axis_l.x
+#    l_y = 0.5 + joy.axis_l.y
     l_x *= 10
     l_y *= 10
     print(l_y, l_x)
