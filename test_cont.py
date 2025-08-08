ef42abf3876f9b000885e6e0470177c693009984
3dJonPrint <jonas.spitzmueller@outlook.de>
2025-08-08 09:15:21 +0200

    not writing

--- test_cont.py
+++ test_cont.py
@@ -2,8 +2,8 @@ from xbox360controller import Xbox360Controller
 
 with Xbox360Controller(0, axis_threshold=0) as joy:
   while True:
-    l_x = joy.axis_l.x
-    l_y = joy.axis_l.y
+    l_x = int(joy.axis_l.x)
+    l_y = int(joy.axis_l.y)
     l_x *= 10
     l_y *= 10
     print(l_x, l_y)
