4e8300f1d1da9deb9b0bf4306c3770743ff5a3af
3dJonPrint <jonas.spitzmueller@outlook.de>
2025-08-08 09:01:08 +0200

    Added test_cont.py to git write ab code in main.py it's chaos

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from board import SCL, SDA
 import busio
 import time
 from time import sleep
+import math
 from adafruit_pca9685 import PCA9685
 from xbox360controller import Xbox360Controller
 
@@ -10,12 +11,14 @@ debug = False
 gamecont = True
 user_in = [0, 0]
 
+move_speed = 1
+
 i2c = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c)
 pca.frequency = 50
 
 pos_us = [0, 0, 0, 0, 0]
-limit = [[500, 900], [500, 2600], [500, 2600], [500, 2600], [500, 2600], [500, 2600]]
+hard_limit = [[500, 900], [500, 2600], [500, 2600], [500, 2600], [500, 2600], [500, 2600]]
 
 def duty_calc(us):
     if type(us) != int:
@@ -25,20 +28,21 @@ def duty_calc(us):
     return(duty)
 
 
-def limit_2_calc():
-    global limit_2
-    limit_2 = [max(-pos_us[1]+3100,limit[2][0]),
-               min(-pos_us[1]+4100, limit[2][1])]
+def limit_calc():
+    global limit
+    limit = [[None, None], [None, None], [None, None], [None, None], [None, None], [None, None]]
+    limit[1] = [max((pos_us[2]-3100)/-1, hard_limit[1][0]),
+                min((pos_us[2]-4100)/-1, hard_limit[1][1])]
+    limit[2] = [max(-pos_us[1]+3100, hard_limit[2][0]),
+                min(-pos_us[1]+4100, hard_limit[2][1])]
+    for i in range(len(limit)):
+        if limit[i] == [None, None]:
+            limit[i] = hard_limit[i]
 
-def move_servo(servo, pos, limit = limit):
+def move_servo(servo, pos):
     global pos_us
-    if servo == 2:
-        limit_2_calc()
-        limit = limit_2
-    else:
-        limit = limit[servo]
-
-    pos = min(limit[1],max(limit[0],pos))
+    limit_calc()
+    pos = min(hard_limit[1],max(hard_limit[0],pos))
     pos_us[servo] = pos
     duty = duty_calc(pos)
     if debug:
@@ -46,6 +50,9 @@ def move_servo(servo, pos, limit = limit):
     else:
         servo = pca.channels[servo]
         servo.duty_cycle = duty
+
+def clean_cont_number(number):
+    number *= 10
 
 def start_pos():
     move_servo(0, 700)
@@ -57,12 +64,16 @@ try:
     if gamecont:
         while True:
             with Xbox360Controller(0, axis_threshold=0) as joy:
-                l_x = 0.5 + joy.axis_l.x
-                l_y = 0.5 - joy.axis_l.y
+                l_x = joy.axis_l.x
+                l_y = joy.axis_l.y
+                l_x
+                l_y
+                l_x *= move_speed
+                l_y *= move_speed
                 print(l_x, l_y)
-                print(pos_us[1]+l_x*10, pos_us[2]+l_y*10)
-                move_servo(1,pos_us[1]+l_x*10)
-                move_servo(2, pos_us[2]+l_y*10)
+                print(pos_us[1]+l_x, pos_us[2]+l_y)
+                move_servo(1,pos_us[1]+l_x)
+                move_servo(2, pos_us[2]+l_y)
 
     else:
         while True:
